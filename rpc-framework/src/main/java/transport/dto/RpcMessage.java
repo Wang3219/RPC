9bdd35e5d2e91268172364ec4bf86b3ad26bc438
suancaiyu <2459145973@qq.com>
2023-05-08 21:39:27 +0800

    创建实体类

---
+++ rpc-framework/src/main/java/transport/dto/RpcMessage.java
@@ -0,0 +1,23 @@
+package transport.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: weiyi.wang1999@qq.com
+ * @create: 2023-05-08 21:35
+ * @Description:
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class RpcMessage {
+    private byte messageType;
+    private byte codec;
+    private byte compress;
+    private int requestId;
+    private Object data;
+}
---
+++ rpc-framework/src/main/java/transport/dto/RpcResponse.java
@@ -0,0 +1,40 @@
+package transport.dto;
+
+import constants.RpcResponseConstant;
+import lombok.*;
+
+import java.io.Serializable;
+
+/**
+ * @author: weiyi.wang1999@qq.com
+ * @create: 2023-05-08 20:49
+ * @Description:
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class RpcResponse<T> implements Serializable {
+    private static final long serialVersionUID = 469636468377226672L;
+    private String requestId;
+    private Integer code;
+    private String message;
+    private T data;
+
+    public static <T> RpcResponse<T> success(T data, String requestId) {
+        RpcResponse<T> response = new RpcResponse<>();
+        response.setCode(RpcResponseConstant.SUCCESS.getCode());
+        response.setMessage(RpcResponseConstant.SUCCESS.getMessage());
+        response.setRequestId(requestId);
+        if (data != null)
+            response.setData(data);
+        return response;
+    }
+
+    public static <T> RpcResponse<T> fail() {
+        RpcResponse<T> response = new RpcResponse<>();
+        response.setCode(RpcResponseConstant.FAIL.getCode());
+        response.setMessage(RpcResponseConstant.FAIL.getMessage());
+        return response;
+    }
+}
